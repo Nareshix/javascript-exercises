a5c5c757bcd0d2446983a7d492f6f91aa2aaf6dc
nareshix <nareshix66@gmail.com>
2024-08-25 21:26:57 +0800

    completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    // if num % 1 === 0, means its an integer, else its a float
+    if (num1 < 0 || num2 < 0 || !Number.isSafeInteger(num1) || !Number.isSafeInteger(num2)){ 
+        return 'ERROR';
+    }
+    bigger = Math.max(num1,num2);
+    smaller = Math.min(num1,num2);
 
+    let total = 0;
+    for (let i = smaller; i <= bigger; ++i){
+        total += i;
+    }
+    return total
 };
 
 // Do not edit below this line
