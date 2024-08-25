3b4a25b67cffefa7756f87d2aaa7f48b14607bda
nareshix <nareshix66@gmail.com>
2024-08-25 20:21:28 +0800

    changed my answer to using only built in methods

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,6 @@
 const reverseString = function(str) {
-    ans = '';
-    for (let i = str.length - 1; i >= 0; --i ){
-        ans += str[i];
-    }
-    return ans;
+    // return str.split().reverse().join('');
+    return str.split('').reverse().join('');
 };
 
 // Do not edit below this line
