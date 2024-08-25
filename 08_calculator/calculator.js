ef57cc70b1639aaea804adcd59a6a7b43b41d286
nareshix <nareshix66@gmail.com>
2024-08-25 22:23:00 +0800

    done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length == 0){
+    return 0;
+  }  
+  return array.reduce((accumulate, num) => accumulate + num);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((accumulate, num) => accumulate * num);
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+  return num1**num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1
+  if (num == 0){
+    return 1;
+  }
+  else{
+    for (let i = 1; i <= num; ++i){
+      total *= i;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
