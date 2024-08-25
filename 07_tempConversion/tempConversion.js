7a4ffe08e6beabad12df5ca9666e08ffe06fda6f
nareshix <nareshix66@gmail.com>
2024-08-25 21:57:42 +0800

    done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+
+  let ans = (num-32)/1.8;
+  return Number(ans.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let ans =num *(9/5) + 32;
+  return Number(ans.toFixed(1));
 };
 
 // Do not edit below this line
@@ -9,3 +14,5 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+
