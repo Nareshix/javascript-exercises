e20699461aa1e7aab1c2ed29abc74bd161bfdd04
nareshix <nareshix66@gmail.com>
2024-08-25 20:08:27 +0800

    comleted using a simple for loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, int) {
+    if (int < 0){
+        return 'ERROR';
+    }
+    
+    let ans = '';
+    for (let i = 0; i < int; ++i){
+        ans += str;
+    }
+    return ans;
 };
 
 // Do not edit below this line
