92efe33f1b298f34628a578c9590b58e44faaa65
nareshix <nareshix66@gmail.com>
2024-08-26 13:33:25 +0800

    completed using a for loop

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let age = 0;
+    let detail;
 
+    
+    for (item of arr){
+        if (typeof item.yearOfDeath === 'undefined'){
+            let today = new Date();
+            age = today.getFullYear() - item.yearOfBirth;
+            detail = item;
+        }
+        if (item.yearOfDeath - item.yearOfBirth > age){
+            age = item.yearOfDeath - item.yearOfBirth;
+            detail = item;
+        }
+    }
+    return detail;
 };
 
 // Do not edit below this line
