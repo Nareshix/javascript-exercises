bec1071bda752edaa812f95cc3308b8bb5aaaa4f
nareshix <nareshix66@gmail.com>
2024-08-26 22:38:26 +0800

    completed using a reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,8 +1,12 @@
+const findAge = person => {
+    if (person.yearOfDeath === undefined){
+        let currentYear = new Date().getFullYear() 
+        return currentYear - person.yearOfBirth; 
+    }
+    return person.yearOfDeath - person.yearOfBirth;
+}
 const findTheOldest = function(arr) {
-    let age = 0;
-    let detail;
-
-    
+    return arr.reduce((oldest, current) => findAge(current) > findAge(oldest) ? current : oldest);
 };
 
 // Do not edit below this line
