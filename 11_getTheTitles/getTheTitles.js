59352c5b42d4cff1903d2e2b3d9004989d3af4a4
nareshix <nareshix66@gmail.com>
2024-08-26 12:57:39 +0800

    completed using a simple loop

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(book) {
+    let ans = [];
+    for (arr of book){
+        ans.push(arr.title);
+    }
+    return ans;
 };
 
 // Do not edit below this line
